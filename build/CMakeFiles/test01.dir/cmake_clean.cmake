b3786eab7180bafd23ac5dbdda7c9c2c5a80f1c0
yinleipeng <504032294@qq.com>
2021-11-13 19:48:14 -0800

    add the seqlist

---
+++ build/CMakeFiles/test01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test01.dir/seqlist.c.o"
+  "CMakeFiles/test01.dir/test.c.o"
+  "test01.pdb"
+  "test01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ seqlist.h
@@ -0,0 +1,21 @@
+
+#ifndef _SEQLIST_H_ 
+#define _SEQLIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef void SeqList;
+typedef void SeqListNode;
+
+SeqList* SeqList_Create(int capacity);                        //创建一个空的线性表
+void SeqList_Destroy(SeqList* list);                          //销毁线性表
+void SeqList_Clear(SeqList* list);                            //清空线性表
+int SeqList_Length(SeqList* list);                            //获取线性表的长度
+int SeqList_Capacity(SeqList* list);                          //获取线性表的容量
+int SeqList_Insert(SeqList* list,SeqListNode* node,int pos);  //将节点插入到某位置
+SeqListNode* SeqList_Get(SeqList* list,int pos);              //获取线性表上的节点
+SeqListNode* SeqList_Delete(SeqList* list,int pos);           //删除某节点
+
+#endif
